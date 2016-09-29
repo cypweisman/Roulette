e9732459b8498edcb6d1451f2ed910891f680c2d
Cy Weisman <cyweisman@gmail.com>
2016-09-29 15:46:20 -0500

    add working yelp objects

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,23 +1,11 @@
 require 'yelp'
+require 'json'
 
 get '/' do
   erb :'/show'
 end
 
 get '/events' do
-  # Yelp.client.configure do |config|
-  # config.consumer_key = ENV["CONSUMER_KEY"]
-  # config.consumer_secret = ENV["CONSUMER_SECRET"]
-  # config.token = ENV["YELP_TOKEN"]
-  # config.token_secret = ENV["YELP_TOKEN_SECRET"]
-  # Yelp.client.search('San Francisco', { term: 'food' })
-  # end
-# client.search(‘Chicago’, search_params_here)
-p "Hellooooooooooo"
-p ENV["CONSUMER_KEY"]
-p ENV["CONSUMER_SECRET"]
-p ENV["YELP_TOKEN"]
-p ENV["YELP_TOKEN_SECRET"]
 
 client = Yelp::Client.new({
   consumer_key: ENV["CONSUMER_KEY"],
@@ -29,19 +17,10 @@ client = Yelp::Client.new({
 
 
 
-p @answers = client.search('San Francisco')
-#p @answers
+@answers = client.search('San Francisco')
+@result = @answers.businesses.sample.name
 
-    # if @answer != []
-      #@answer = city.sample.uri
-      #@answer = @answers[0].name
-      #p @answer[0]
-    #else
-      # artist = 'Cher'
-      # @uri = RSpotify::Artist.search(artist).sample.uri
-      #return false
-    # end
-
+    # redirect "/"
   erb :'/show'
 
 end
